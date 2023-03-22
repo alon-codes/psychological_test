85eae75f685ac409828f48cbd0fe88045a6d2dd0
Alon Dubov <alon@dubov.tech>
2023-03-22 02:35:19 +0200

    Fixing routes path

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
-import Quiz from './quiz';
+import Quiz from './routes/quiz';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Score from './score';
+import Score from './routes/score';
 import { Container } from '@mui/system';
 import Header from './header';
 import HomeRoute from './routes/home';
