a2c07139243ffdc6ea4a453273b177bee28ee533
Alon Dubov <alon@alon.codes>
2023-08-22 16:24:49 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,6 +10,14 @@ async function bootstrap() {
     .setDescription('Quiz API documention')
     .setVersion('1.0')
     .build();
+
+  app.enableCors({
+    origin: ['https://psychological-test-react-client.vercel.app', 'http://localhost:3000'],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
