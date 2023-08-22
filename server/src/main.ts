1a530293a3aca79239034c4ec73e44ae47f4d133
Alon Dubov <alon@alon.codes>
2023-08-22 16:25:00 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Quiz API documention')
