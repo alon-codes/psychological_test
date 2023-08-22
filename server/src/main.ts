bd9c829be0e04bfd7577c77442b00bdec4986522
Alon Dubov <alon@alon.codes>
2023-08-22 16:35:16 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,13 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  app.enableCors({
-    origin: ['https://psychological-test-react-client.vercel.app', 'http://localhost:3000'],
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-  });
+  app.enableCors();
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
