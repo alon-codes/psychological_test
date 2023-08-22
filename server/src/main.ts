36e9109b05e70aa9235deeb7a1132db52db196e3
Alon Dubov <alon@alon.codes>
2023-08-22 14:48:36 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  app.enableCors();
+  app.enableCors({ origin: "*" });
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
