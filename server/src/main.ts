e93f60b1c496044aab9ba93c4efde5c9e0b91269
Alon Dubov <alon@alon.codes>
2023-08-22 15:53:00 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,11 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  app.enableCors({
-    origin: "https://psychological-test-react-client.vercel.app",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": true
-  });
+  app.enableCors();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
