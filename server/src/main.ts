b89825381fae4cf6fdcbd5177d1c1a269c45a87d
Alon Dubov <alon@dubov.tech>
2023-08-23 17:06:07 +0300

    Updating options response status to 200

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,18 +3,21 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      methods: ['GET', 'PUT', 'POST', 'PATCH', 'OPTIONS'],
+      origin: '*',
+      exposedHeaders: 'Access-Control-Allow-Origin',
+      allowedHeaders: '*',
+      optionsSuccessStatus: 200
+    }
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Quiz API documention')
     .setDescription('Quiz API documention')
     .setVersion('1.0')
     .build();
-
-  app.enableCors({
-    methods: ['GET', 'PUT', 'POST', 'PATCH', 'OPTIONS'],
-    origin: '*',
-  });
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
