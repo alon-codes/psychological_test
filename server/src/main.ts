bc37e9259eb75159450cceaf1f2d2a1c3326d9ec
Alon Dubov <alon@dubov.tech>
2023-08-23 14:10:37 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,19 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Quiz API documention')
     .setDescription('Quiz API documention')
     .setVersion('1.0')
     .build();
+
+  app.enableCors({
+    //methods: ['GET', 'PUT', 'POST', 'PATCH', 'OPTIONS', 'HEAD'],
+    origin: ['https://psychological-test-react-client.vercel.app'],
+    preflightContinue: true,
+  });
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
