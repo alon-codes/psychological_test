32fe818e24210cc89e3b737f0b6b379537554059
Alon Dubov <alon@dubov.tech>
2023-08-24 12:47:08 +0300

    Removing exposed endpoints

--- server/src/quiz/quiz.controller.ts
+++ server/src/quiz/quiz.controller.ts
@@ -1,54 +1,15 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { QuizService } from './quiz.service';
-import { CreateQuestionDto } from './dto/create-question.dto';
-import { UpdateQuestionDto } from './dto/update-question.dto';
 import { SubmitQuizDto } from './dto/submit-quiz-dto';
 
 @Controller('quiz')
 export class QuizController {
-  constructor(private readonly questionService: QuizService) {}
+  constructor(private readonly questionService: QuizService) { }
 
-  @Post()
-  async create(@Body() createQuestionDto: CreateQuestionDto) {
-    try {
-      const res = await this.questionService.create(createQuestionDto);
-      return res;
-    } catch (e) {
-      console.log({ e });
-    }
-
-    return null;
-  }
 
   @Get()
   async findAll() {
     return await this.questionService.findAll();
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.questionService.getQuestionById(id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateQuestionDto: UpdateQuestionDto,
-  ) {
-    return this.questionService.update(id, updateQuestionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.questionService.remove(+id);
   }
 
   @Post('submit')
